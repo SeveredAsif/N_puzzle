0d3e74b9814a7b832700903bd7449d4309156997
SeveredAsif <109129903+SeveredAsif@users.noreply.github.com>
2025-04-17 15:47:35 +0600

    array print

--- search.py
+++ search.py
@@ -5,7 +5,7 @@ node = Node([1,2,3,4,0,5,6,7,8])
 def find_moves(node,n):
     ans = []
     arr = node.initial_config
-    print(arr)
+    #print(arr)
     row = 0
     col = 0
     zero_pos = 0
@@ -14,7 +14,7 @@ def find_moves(node,n):
             row = i//n 
             col = i%n
             zero_pos = i
-            print(zero_pos)
+            #print(zero_pos)
             break
     
     poss_moves = [[row-1,col],[row+1,col],[row,col-1],[row,col+1]]
@@ -26,7 +26,7 @@ def find_moves(node,n):
             duplicate_arr[zero_pos] = temp
             duplicate_arr[poss_move[0]*n+poss_move[1]] = 0
             ans.append(duplicate_arr)
-            print(duplicate_arr)
+            #print(duplicate_arr)
     return ans
 
 #find_moves(node,3)
@@ -55,6 +55,7 @@ def A_star_search(node,n):
     while (len(h)):
         child_node = heapq.heappop(h)[1]
         #print(nodee)
+        print_node(child_node,n)
         if(np.array_equal(intended,child_node.initial_config)):
             print(f"Minimum number of moves: {child_node.moves}")
             print(f"Expanded: {len(expanded)}")
@@ -63,7 +64,7 @@ def A_star_search(node,n):
         expanded.append(child_node)
         # print(nodee[1].initial_config)
         possible_nodes  = find_moves(child_node,n)
-        print(possible_nodes)
+        #print(possible_nodes)
         for possible_node in possible_nodes:
             moves = child_node.moves + 1
             new_node = Node(possible_node)
@@ -71,10 +72,20 @@ def A_star_search(node,n):
             new_node.parent = child_node
             new_node.priority = moves + manhattan_distance(new_node,n)
             explored.append(new_node)
+            # print_node(new_node,n)
             heapq.heappush(h,(new_node.priority,new_node))
 
 
 node = Node([0,1,3,4,2,5,7,8,6])
+
+def print_node(node,n):
+    # arr = np.array(node.initial_config)
+    # arr = np.split(arr,n,axis=0)
+    arr = np.array(node.initial_config).reshape(n, n)
+    print(arr)
+    print()
+    print()
+
 
 A_star_search(node,3)
     
